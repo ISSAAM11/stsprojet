3de4077c47a194c404224b8b61e83a7ed8c74177
ben hssin <benhassineissam@gmail.com>
2021-03-26 12:37:29 +0100

    test

---
+++ stsGitHub/src/main/java/stsGitHub/stsGitHub/App.java
@@ -0,0 +1,13 @@
+package stsGitHub.stsGitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
